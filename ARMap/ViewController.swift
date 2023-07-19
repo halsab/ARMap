7bac21cb9589e8dd793e6a5b097d03be47e1044c
khalil_sabirov <SabirovKhA@mosreg.ru>
2023-07-19 17:14:40 +0300

    Base vc setup

--- ARMap/ViewController.swift
+++ ARMap/ViewController.swift
@@ -6,14 +6,45 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var mapView: MKMapView!
+    @IBOutlet private weak var cameraButton: UIButton!
+
+    private let locationManager = CLLocationManager()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setup()
     }
 
-
+    private func setup() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.startUpdatingLocation()
+        locationManager.requestWhenInUseAuthorization()
+    }
 }
 
+// MARK: - Action
+
+extension ViewController {
+    @IBAction func showARController(_ sender: Any) {
+
+    }
+}
+
+// MARK: - CLLocationManagerDelegate
+
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+
+    }
+}
